79a87f14f2cb8baf5a91eef0c85dceaac99efcbf
Lexo783 <florentdebuchy@orange.fr>
2020-12-09 13:23:43 +0100

    add

--- src/Controller/Admin/MovieCrudController.php
+++ src/Controller/Admin/MovieCrudController.php
@@ -2,21 +2,13 @@
 
 namespace App\Controller\Admin;
 
-use App\EasyAdminCustomFields\CollectionEntityGenre;
-use App\Entity\Genre;
 use App\Entity\Movie;
-use Doctrine\ORM\Cache\AssociationCacheEntry;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-<<<<<<< Updated upstream
-=======
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
->>>>>>> Stashed changes
 
 class MovieCrudController extends AbstractCrudController
 {
@@ -25,15 +17,11 @@ class MovieCrudController extends AbstractCrudController
         return Movie::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
-<<<<<<< Updated upstream
-            TextEditorField::new('description'),
-=======
             TextEditorField::new('plot'),
             ChoiceField::new('rated')
                 ->setChoices([
@@ -48,8 +36,6 @@ class MovieCrudController extends AbstractCrudController
             //CollectionEntityGenre::new('genres'),
             //ImageField::new('picture'),
             TextField::new('type'),
->>>>>>> Stashed changes
         ];
     }
-    */
 }
