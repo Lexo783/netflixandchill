df55520322b565b7513e6f9a8a6d43afb66d9579
Dreaxxx <ldqtheone@gmail.com>
2020-12-10 17:22:16 +0100

    prepare favorites

---
+++ src/Controller/FavoriteController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Favorite;
+use App\Entity\User;
+use App\Repository\FavoriteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FavoriteController extends AbstractController
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    #[Route('/favorite', name: 'favorite')]
+    public function index(): Response
+    {
+        return $this->render('favorite/index.html.twig', [
+            'controller_name' => 'FavoriteController',
+        ]);
+    }
+
+    #[Route('/favorite/{action}/{filmId}', name: 'toggle_favorite')]
+    public function toggleFavorite($action, $filmId, FavoriteRepository $favoriteRepository)
+    {
+        $user = $this->getUser();
+
+        $favoriteList = $user->getFavorites();
+
+        $favorite = $favoriteRepository->findOneBy(
+            [
+                'user' => $user,
+                'movie' => $filmId
+            ]
+        );
+
+        if(!in_array($filmId, $favoriteList)) {
+            $newFavorite = new Favorite();
+            $newFavorite->setUser($user);
+            $newFavorite->setMovie($filmId);
+
+            $this->em->persist($newFavorite);
+            $this->em->flush();
+
+            return true;
+        }
+        else{
+            $user->removeFavorite($favorite);
+            
+            return false;
+        }
+    }
+}
