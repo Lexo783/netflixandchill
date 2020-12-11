5089770ccd7870677d6b7321a599261c0a7f0547
Dreaxxx <ldqtheone@gmail.com>
2020-12-11 16:54:02 +0100

    add route for api login/register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\AppAuthAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +19,7 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, AppAuthAuthenticator $authenticator): Response
     {
         $user = new User();
+
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
@@ -46,5 +48,40 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/registerElectron', name: 'app_register_electron')]
+    public function registerElectron(Request $request, UserPasswordEncoderInterface $passwordEncoder): JsonResponse
+    {
+        $user = new User();
+
+        $form = $request->query->all();
+
+        if ($form) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $request->query->get('password')
+                )
+            );
+
+            $user->setPicture($request->query->get('picture'));
+            $user->setBirthday(new \DateTime($request->query->get('birthday')));
+            $user->setEmail($request->query->get('email'));
+            $user->setFirstName($request->query->get('firstName'));
+            $user->setLastName($request->query->get('lastName'));
+            $user->setPseudo($request->query->get('pseudo'));
+            $user->setRoles(["ROLE_USER"]);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return new JsonResponse("1");
+        }
+
+        return new JsonResponse("0");
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->entityManager = $entityManager;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -24,6 +39,31 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/loginElectron", name="app_login_electron")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function loginElectron(Request $request): JsonResponse
+    {
+        $email = $request->query->get('email');
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            // fail authentication with a custom error
+            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+        }
+
+        $password = $this->passwordEncoder->isPasswordValid($user, $request->query->get('password'));
+
+        if($password){
+            return new JsonResponse("1");
+        }
+
+        return new JsonResponse("0");
     }
 
     /**
