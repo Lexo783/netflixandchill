4630af607fc0ccffd9d788b00e8577826049d55e
CHALET <emerick.chalet@edu.itescia.fr>
2020-12-13 23:00:19 +0100

    register done

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,9 +19,15 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email',EmailType::class,[
+                'label'=> false,
+                'attr' => [
+                    'class' => 'textbox'
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label'=> false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -29,6 +37,10 @@ class RegistrationFormType extends AbstractType
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label'=> false,
+                'attr' => [
+                    'class' => 'textbox'
+                ],
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
