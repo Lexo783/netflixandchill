6585562135df1ebf3573c4c1223d1bf8658d3582
CHALET <emerick.chalet@edu.itescia.fr>
2020-12-17 10:02:59 +0100

    profil begin front

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\SearchType;
 use App\Repository\MovieRepository;
+use App\Services\Profile;
 use App\Services\Search;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(Request $request,MovieRepository $movieRepository): Response
+    public function index(Request $request,MovieRepository $movieRepository, Profile $profile): Response
     {
         $search = new Search();
         $form = $this->createForm(SearchType::class,$search);
@@ -27,6 +28,7 @@ class HomeController extends AbstractController
         }
         return $this->render('home/index.html.twig', [
             'movies' => $movies,
+            'profile' => $profile->getProfile()
         ]);
     }
 }
